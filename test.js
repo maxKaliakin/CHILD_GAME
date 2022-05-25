1ee017a216c6a52cf57ef33d4af84f34d1eaf928
Max Kaliakin <sakurwork@gmail.com>
2022-05-25 11:04:15 +0300

    add WORD

--- test.js
+++ test.js
@@ -34,12 +34,29 @@ const russianAlphabet = [
   'я',
 ];
 
+const data = {
+  word: 'лошадь',
+  image: 'http:///asdasd/horse.jpg',
+  video: 'http:///asdasd/horse.mp4',
+};
+
+const wordCreate = (slovo) => {};
+
+wordCreate(data.word);
+
 russianAlphabet.forEach((el) => {
   const button = document.createElement('button');
   button.className = 'keypadButton';
-  console.log('YO');
   button.type = 'button';
   button.addEventListener('click', () => console.log(el));
-  button.textContent = JSON.stringify(el);
+  button.textContent = el;
   document.getElementById('keypad').appendChild(button);
 });
+
+// function getRandomInt(chislo) {
+//   return Math.floor(Math.random() * chislo);
+// }
+
+// const word = slovo.length; // 10
+
+// console.log(Math.floor(Math.random() * data.word.length));
