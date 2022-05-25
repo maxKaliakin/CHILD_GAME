a1dec03a84741c54e1f87c5484c13e6b9b8727d3
Max Kaliakin <sakurwork@gmail.com>
2022-05-25 10:15:41 +0300

    Add keypad

---
+++ test.js
@@ -0,0 +1,44 @@
+const russianAlphabet = [
+  'а',
+  'б',
+  'в',
+  'г',
+  'д',
+  'е',
+  'ё',
+  'ж',
+  'з',
+  'и',
+  'й',
+  'к',
+  'л',
+  'м',
+  'н',
+  'о',
+  'п',
+  'р',
+  'с',
+  'т',
+  'у',
+  'ф',
+  'х',
+  'ц',
+  'ч',
+  'ш',
+  'щ',
+  'ъ',
+  'ы',
+  'ь',
+  'э',
+  'ю',
+  'я',
+];
+
+russianAlphabet.forEach((el) => {
+  const button = document.createElement('button');
+  button.className = 'keypadButton';
+  button.type = 'button';
+  button.addEventListener('click', () => console.log(el));
+  button.textContent = JSON.stringify(el);
+  document.getElementById('keypad').appendChild(button);
+});
