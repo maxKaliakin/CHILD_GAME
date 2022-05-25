0fb247729b63f0d02989f147ea30d595e98714b0
Max Kaliakin <sakurwork@gmail.com>
2022-05-25 14:07:00 +0300

    Update HTML and OPEN func for success result

--- test.js
+++ test.js
@@ -40,15 +40,70 @@ const data = {
   video: 'http:///asdasd/horse.mp4',
 };
 
+let lettersArray = data.word.split('');
+let secretWord = new Array(data.word.length).fill('');
+
+// Получаем рендомный номер (number)
 const getRandomNumber = (number) => {
   return Math.floor(Math.random() * number);
 };
+
+// Открываем рендомный кусок картинки
+const openRandomImagePart = () => {
+  console.log(
+    'Открываем этот кусочек ===>>>',
+    getRandomNumber(lettersArray.length),
+  );
+  // TODO:  Добавить проверку - была ли цифра уже открыта
+};
+
+// Открываем букву на табло
+const openRightLetter = () => {
+  // TODO: Перерисовываем слово на табло с учетом открытых букв в secretWord
+};
+
+const checkLetterInSecretWord = (letter) => {
+  // Проверяем есть ли буква в массиве букв из отгадываемого слова
+  if (lettersArray.includes(letter)) {
+    //Если да - проверяем по какому индексу и добавляем сразу в скрытое слово
+    lettersArray.findIndex((element, index) => {
+      if (element === letter) {
+        secretWord[index] = letter;
+        openLetter(index, letter);
+        openRandomImagePart();
+      }
+    });
+  }
+  // Если нет - просто говорим что FAIL
+  else {
+    console.log('FAIl', secretWord);
+  }
+};
+
+// Отрисовываем слово, которое будем отгадывать
+const renderLetters = () => {
+  return secretWord.forEach((el) => {
+    const letterBox = document.createElement('li');
+    letterBox.className = el ? 'open' : '';
+    letterBox.textContent = el;
+    document.getElementById('word').appendChild(letterBox);
+  });
+};
+
+// Открываем уже в HTML букву по индексу
+const openLetter = (index, letter) => {
+  const element = document.getElementById('word').childNodes[index];
+  element.className = 'open';
+  element.textContent = letter;
+};
+
+renderLetters();
 
 russianAlphabet.forEach((el) => {
   const button = document.createElement('button');
   button.className = 'keypadButton';
   button.type = 'button';
-  button.addEventListener('click', () => console.log(el));
+  button.addEventListener('click', () => checkLetterInSecretWord(el));
   button.textContent = el;
   document.getElementById('keypad').appendChild(button);
 });
